f3ea6df99fcf20ebd9bbba2a917faf34b91a9d68
DevExpressExampleBot <fakeEmail>
2022-12-07 15:54:17 +0400

    Source auto update [skip ci]

--- VB/Default.aspx.vb
+++ VB/Default.aspx.vb
@@ -9,7 +9,7 @@ Imports System.Web.UI.WebControls
 Imports System.Web.UI.WebControls.WebParts
 Imports System.Web.UI.HtmlControls
 Imports System.Xml
-Imports DevExpress.Web.ASPxNavBar
+Imports DevExpress.Web
 
 Partial Public Class _Default
     Inherits System.Web.UI.Page
